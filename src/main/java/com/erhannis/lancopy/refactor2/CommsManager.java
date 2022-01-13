15495827c450ab9763bcfc2809932fe82af51b25
erhannis <eyeillus@gmail.com>
2022-01-12 23:13:42 -0500

    Ok, we're briefly near a stable point for CommsManager

--- src/main/java/com/erhannis/lancopy/refactor2/CommsManager.java
+++ src/main/java/com/erhannis/lancopy/refactor2/CommsManager.java
@@ -24,6 +24,7 @@ import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jcsp.helpers.JcspUtils;
+import jcsp.helpers.SynchronousSplitter;
 import jcsp.lang.Alternative;
 import jcsp.lang.AltingChannelInput;
 import jcsp.lang.Any2OneChannel;
@@ -45,9 +46,17 @@ public class CommsManager implements CSProcess {
 
     private final DataOwner dataOwner;
     
-    public CommsManager(DataOwner dataOwner) {
+    public CommsManager(DataOwner dataOwner, ChannelOutput<Comm> lcommOut, ChannelOutput<Advertisement> radOut, AltingChannelInput<Advertisement> aadIn, ChannelOutput<Summary> rsumOut, AltingChannelInput<Summary> lsumIn, ChannelOutput<Pair<Comm,Boolean>> statusOut, AltingChannelInput<List<Comm>> subscribeIn) {
         this.dataOwner = dataOwner;
 
+        this.lcommOut = lcommOut;
+        this.radOut = radOut;
+        this.aadIn = aadIn;
+        this.rsumOut = rsumOut;
+        this.lsumIn = lsumIn;
+        this.statusOut = statusOut;
+        this.subscribeIn = subscribeIn;
+        
         Any2OneChannel<Pair<NodeManager.CRToken,byte[]>> internalRxMsgChannel = Channel.<Pair<NodeManager.CRToken,byte[]>> any2one();
         this.internalRxMsgIn = internalRxMsgChannel.in();
         this.internalRxMsgOut = JcspUtils.logDeadlock(internalRxMsgChannel.out());
@@ -59,6 +68,9 @@ public class CommsManager implements CSProcess {
         Any2OneChannel<Pair<Comm,Boolean>> internalCommStatusChannel = Channel.<Pair<Comm,Boolean>> any2one();
         this.internalCommStatusIn = internalCommStatusChannel.in();
         this.internalCommStatusOut = JcspUtils.logDeadlock(internalCommStatusChannel.out());
+        
+        this.broadcastMsgSplitter = new SynchronousSplitter<byte[]>();
+        this.txMsgSplitter = new SynchronousSplitter<byte[]>();
     }
 
     private final ChannelOutput<Comm> lcommOut;
@@ -74,14 +86,19 @@ public class CommsManager implements CSProcess {
     private final ChannelOutput<NodeManager.ChannelReader> internalChannelReaderShuffleAOut;
     private final AltingChannelInput<Pair<Comm,Boolean>> internalCommStatusIn;
     private final ChannelOutput<Pair<Comm,Boolean>> internalCommStatusOut;
+    private final SynchronousSplitter<byte[]> broadcastMsgSplitter;
+    private final SynchronousSplitter<byte[]> txMsgSplitter;
 
     private HashMap<UUID, NodeManager.NMInterface> nodes = new HashMap<>();
     
-    private NodeManager.NMInterface startNodeManager(UUID id) {
+    private NodeManager.NMInterface startNodeManager(UUID id, boolean registerToSplitter) {
         // Blehhhhh, this is boilerplatey
         Any2OneChannel<byte[]> txMsgChannel = Channel.<byte[]> any2one();
         AltingChannelInput<byte[]> txMsgIn = txMsgChannel.in();
         ChannelOutput<byte[]> txMsgOut = JcspUtils.logDeadlock(txMsgChannel.out());
+        if (registerToSplitter) {
+            this.txMsgSplitter.register(txMsgOut);
+        }
 
         
         Any2OneChannel<NodeManager.CRToken> demandShuffleChannelChannel = Channel.<NodeManager.CRToken> any2one();
@@ -101,7 +118,7 @@ public class CommsManager implements CSProcess {
         AltingChannelInput<List<Comm>> subscribeIn = subscribeChannel.in();
         ChannelOutput<List<Comm>> subscribeOut = JcspUtils.logDeadlock(subscribeChannel.out());
 
-        
+                
         new ProcessManager(new NodeManager(dataOwner, id, txMsgIn, internalRxMsgOut, demandShuffleChannelIn, internalChannelReaderShuffleAOut, channelReaderShuffleBIn, incomingConnectionIn, subscribeIn, internalCommStatusOut)).start();
         NodeManager.NMInterface nmi = new NodeManager.NMInterface(txMsgOut, internalRxMsgIn, demandShuffleChannelOut, internalChannelReaderShuffleAIn, channelReaderShuffleBOut, incomingConnectionOut, subscribeOut, internalCommStatusIn);
         nodes.put(id, nmi);
@@ -132,10 +149,13 @@ public class CommsManager implements CSProcess {
                         }
                     }
                 }
-            }, () -> {
-                
-            }
+            },
+            this.broadcastMsgSplitter,
+            this.txMsgSplitter
         })).start();
+        
+        // Start NM to hold incame connections
+        startNodeManager(null, false);
         
         //TODO Add verification to make sure nodes' claims match their TLS credentials
         //DO Read Identification message first off, transfer channel
@@ -144,95 +164,96 @@ public class CommsManager implements CSProcess {
         
         Alternative alt = new Alternative(new Guard[]{aadIn, lsumIn, subscribeIn, internalRxMsgIn, internalChannelReaderShuffleAIn, internalCommStatusIn, internalCommChannelIn});
         while (true) {
-            switch (alt.priSelect()) {
-                case 0: { // aadIn
-                    Advertisement ad = aadIn.read();
-                    byte[] msg = dataOwner.serialize(ad);
-                    
-                    // Broadcast local ad
-                    if (Objects.equals(ad.id, dataOwner.ID)) {
-                        //System.out.println("BroadcastAdvertiser  txb " + MeUtils.cleanTextContent(new String(msg), "�"));
-                        broadcastMsgOut.accept(msg);
-                    }
-                    
-                    //DO Parallel
-                    // Tx Ad to connected nodes
-                    for (Entry<UUID, NodeManager.NMInterface> e : nodes.entrySet()) {
-                        e.getValue().txMsgOut.write(msg);
-                    }
-                    break;
-                }
-                case 1: { // lsumIn
-                    //DO Cache, and autosend to new connections?
-                    Summary summary = lsumIn.read();
-                    byte[] msg = dataOwner.serialize(summary);
-                    for (Entry<UUID, NodeManager.NMInterface> e : nodes.entrySet()) {
-                        if (e.getKey() != null) {
-                            e.getValue().txMsgOut.write(msg);
+            try {
+                switch (alt.priSelect()) {
+                    case 0: { // aadIn
+                        Advertisement ad = aadIn.read();
+                        byte[] msg = dataOwner.serialize(ad);
+
+                        // Broadcast local ad
+                        if (Objects.equals(ad.id, dataOwner.ID)) {
+                            //System.out.println("BroadcastAdvertiser  txb " + MeUtils.cleanTextContent(new String(msg), "�"));
+                            broadcastMsgSplitter.write(msg);
                         }
+
+                        // Tx Ad to connected nodes
+                        //DO Skip null keys?
+                        this.txMsgSplitter.write(msg);
+                        break;
                     }
-                    break;
-                }
-                case 2: { // subscribeIn
-                    List<Comm> comms = subscribeIn.read();
-                    FactoryHashMap<UUID, ArrayList<Comm>> separated = new FactoryHashMap<UUID, ArrayList<Comm>>((input) -> {
-                        return new ArrayList<Comm>();
-                    });
-                    for (Comm comm : comms) {
-                        separated.get(comm.owner.id).add(comm);
+                    case 1: { // lsumIn
+                        //DO Cache, and autosend to new connections?
+                        Summary summary = lsumIn.read();
+                        byte[] msg = dataOwner.serialize(summary);
+                        //DO Skip null keys?
+                        this.txMsgSplitter.write(msg);
+                        break;
                     }
-                    for (UUID id : separated.keySet()) {
-                        if (nodes.containsKey(id)) {
-                            nodes.get(id).subscribeOut.write(separated.get(id));
+                    case 2: { // subscribeIn
+                        List<Comm> comms = subscribeIn.read();
+                        FactoryHashMap<UUID, ArrayList<Comm>> separated = new FactoryHashMap<UUID, ArrayList<Comm>>((input) -> {
+                            return new ArrayList<Comm>();
+                        });
+                        for (Comm comm : comms) {
+                            separated.get(comm.owner.id).add(comm);
+                        }
+                        for (UUID id : separated.keySet()) {
+                            if (nodes.containsKey(id)) {
+                                nodes.get(id).subscribeOut.write(separated.get(id));
+                            } else {
+                                startNodeManager(id, true);
+                                nodes.get(id).subscribeOut.write(separated.get(id));
+                            }
+                        }
+                        break;
+                    }
+                    case 3: { // internalRxMsgIn
+                        Pair<NodeManager.CRToken,byte[]> msg = internalRxMsgIn.read();
+                        Object o = dataOwner.deserialize(msg.b);
+                        if (o instanceof IdentificationMessage) {
+                            IdentificationMessage im = (IdentificationMessage) o;
+                            if (!Objects.equals(im.nodeId, msg.a.nodeId)) {
+                                // Wrong NodeManager; shuffle to correct
+                                msg.a.nodeId = im.nodeId;
+                                nodes.get(msg.a.nodeId).demandShuffleChannelOut.write(msg.a);
+                            }
+                        } else if (o instanceof Advertisement) {
+                            Advertisement ad = (Advertisement) o;
+                            radOut.write(ad);
+                        } else if (o instanceof Summary) {
+                            Summary rsum = (Summary) o;
+                            rsumOut.write(rsum);
                         } else {
-                            startNodeManager(id);
-                            nodes.get(id).subscribeOut.write(separated.get(id));
+                            dataOwner.errOnce("ERR CommsManager got unhandled msg: " + o);
                         }
+                        DO;
+                        // Request data: get data, add to list, set timer to 0 while list, on timer write chunks
+                        break;
                     }
-                    break;
-                }
-                case 3: { // internalRxMsgIn
-                    //DO WRONG; read from the NMIs instead...?
-                    Pair<NodeManager.CRToken,byte[]> msg = internalRxMsgIn.read();
-                    Object o = dataOwner.deserialize(msg.b);
-                    if (o instanceof IdentificationMessage) {
-                        IdentificationMessage im = (IdentificationMessage) o;
-                        if (!Objects.equals(im.nodeId, msg.a.nodeId)) {
-                            // Wrong NodeManager; shuffle to correct
-                            msg.a.nodeId = im.nodeId;
-                            nodes.get(msg.a.nodeId).demandShuffleChannelOut.write(msg.a);
+                    case 4: { // internalChannelReaderShuffleAIn
+                        NodeManager.ChannelReader cr = internalChannelReaderShuffleAIn.read();
+                        UUID id = cr.token.nodeId;
+                        if (!nodes.containsKey(id)) {
+                            startNodeManager(id, true);
                         }
-                    } else if (o instanceof Advertisement) {
-                        Advertisement ad = (Advertisement) o;
-                        radOut.write(ad);
-                    } else if (o instanceof Summary) {
-                        Summary rsum = (Summary) o;
-                        rsumOut.write(rsum);
+                        nodes.get(id).channelReaderShuffleBOut.write(cr);
                     }
-                    DO;
-                    // Request data: get data, add to list, set timer to 0 while list, on timer write chunks
-                    break;
-                }
-                case 4: { // internalChannelReaderShuffleAIn
-                    NodeManager.ChannelReader cr = internalChannelReaderShuffleAIn.read();
-                    UUID id = cr.token.nodeId;
-                    if (!nodes.containsKey(id)) {
-                        startNodeManager(id);
+                    case 5: { // internalCommStatusIn
+                        Pair<Comm,Boolean> status = internalCommStatusIn.read();
+                        //TODO Status of incame connections, too?
+                        statusOut.write(status);
+                        break;
                     }
-                    nodes.get(id).channelReaderShuffleBOut.write(cr);
+                    case 6: { // internalCommChannelIn
+                        CommChannel cc = internalCommChannelIn.read();
+                        // We don't know which node connected to us, yet, so it goes to the blank NM
+                        nodes.get(null).incomingConnectionOut.write(cc);
+                        break;
+                    }
                 }
-                case 5: { // internalCommStatusIn
-                    Pair<Comm,Boolean> status = internalCommStatusIn.read();
-                    //TODO Status of incame connections, too?
-                    statusOut.write(status);
-                    break;
-                }
-                case 6: { // internalCommChannelIn
-                    CommChannel cc = internalCommChannelIn.read();
-                    // We don't know which node connected to us, yet, so it goes to the blank NM
-                    nodes.get(null).incomingConnectionOut.write(cc);
-                    break;
-                }
+            } catch (Throwable t) {
+                System.err.println("CommsManager got error; continuing");
+                t.printStackTrace();
             }
         }
     }
